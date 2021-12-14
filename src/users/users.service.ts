203ac66b23b53332bf5b792da495c9104fd30c5f
luiscostalafc <Nanico2406>
2021-12-14 11:49:12 -0300

    feat: create crud

---
+++ src/users/users.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  private users: User[] = [
+    {
+      id: 1,
+      name: 'Luis Costa',
+      email: 'luis.costa@mail.com',
+    },
+  ];
+
+  create(createUserDto: CreateUserDto) {
+    const id = this.users[this.users.length - 1].id + 1;
+
+    const user: User = {
+      id,
+      ...createUserDto,
+    };
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return this.users.find((user: User) => user.id === id);
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
